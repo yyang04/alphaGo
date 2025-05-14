c154ccae58277e6b113b109c8e37fa0f9f82c439
yufeng.yang <yang.yu.feng.is@gmail.com>
2025-05-14 22:07:02 +0800

    big change

--- utils/date_util.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta
-
-
-def get_days_delta(dt, delta):
-    date_obj = datetime.strptime(dt, '%Y%m%d')
-    return (date_obj + timedelta(days=delta)).strftime('%Y%m%d')
--- utils/udf_util.py
+++
@@ -1,3 +0,0 @@
-def str_magic(x: str) -> str:
-    return f"{x}_magic"
-
